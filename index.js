01ce901fb9016f1e4b46f00b76a3fb56ab9706a0
kylebell8 <kyle.bell2001@hotmail.com>
2022-12-17 05:25:38 +0000

    deployment still not working need to change the proxy stuff in package for frontend as well as app.js to use a different thing instead of 3001

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use('/api', router);
 
 const PORT = process.env.PORT || 3001;
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
