dd269c5086ad117d04023442ea099087bb7f8192
kylebell8 <kyle.bell2001@hotmail.com>
2022-12-17 04:13:13 +0000

    testing deployment further

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
 
 const cors = require('cors');
 app.use(cors());
 
-const path = require('path');
-
-const dirname = path.dirname(__filename);
-
-const bodyParser = require('body-parser');
+const bodyParser  = require('body-parser');
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false }));
 
 const router = require('./routes/routes');
 app.use('/', router);
 
-app.use(express.static(path.join(dirname, 'cd /frontend/build')))
-
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(dirname + 'cd /frontend/build/index.html'))
-})
-
-app.listen(process.env.PORT || 3001, () => {
+app.listen(3001, () => {
   console.log("Server started on port 3001. Ctrl^c to quit.");
 });
