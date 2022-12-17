ec654ce1e6f62834e8387f156cb492ddbfdd7b1b
kylebell8 <kyle.bell2001@hotmail.com>
2022-12-17 02:21:10 +0000

    fixing index

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-
-const path = require('path');
-const dirname = path.dirname(__filename);
 const express = require("express");
 const app = express();
 
+
 const cors = require('cors');
 app.use(cors());
-
 
 const bodyParser  = require('body-parser');
 app.use(bodyParser.json());
@@ -15,13 +12,6 @@ app.use(express.urlencoded({extended: false }));
 const router = require('./routes/routes');
 app.use('/', router);
 
-app.use(express.static(path.join(dirname, "frontend", "build")))
-
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(dirname, "frontend", "build", "index.html"));
-})
-
-app.listen(process.env.port || 3001, () => {
+app.listen(3001, () => {
   console.log("Server started on port 3001. Ctrl^c to quit.");
 });
