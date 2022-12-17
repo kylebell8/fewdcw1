774d7e2d405c19a8acaae8b7b5d1ca06226c5357
kylebell8 <kyle.bell2001@hotmail.com>
2022-12-17 05:28:43 +0000

    deployment still not working need to change the proxy stuff in package for frontend as well as app.js to use a different thing instead of 3001

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ const bodyParser  = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: false }));
 
-app.use(express.static(path.resolve(__dirname, '/fronted/build')));
+
 
 const router = require('./routes/routes');
 app.use('/api', router);
+
+app.use(express.static(path.resolve(__dirname, 'frontend','build')));
 
 const PORT = process.env.PORT || 3001;
 
@@ -25,6 +27,7 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/frontend/build', 'index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'frontend','build', 'index.html');
+  res.sendFile(index);
 });
